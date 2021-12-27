2d2fe6818e31a67448a68da616b055e9905830e5
AmalkrishnaAS <amalkrishna02016@gmail.com>
2021-12-27 17:14:29 +0530

    dob added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import os
+import datetime
 
 
 
@@ -18,16 +19,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 class User(db.Model):
     
     rollno = db.Column(db.String(50),primary_key=True)
+    name = db.Column(db.String(50))
     mobile=db.Column(db.String(10))
     branch=db.Column(db.String(10))
     email=db.Column(db.String(10))
     sex=db.Column(db.String(50))
-    age=db.Column(db.Integer)
+    dob=db.Column(db.DateTime)
     
 
 class User_schema(ma.Schema):
     class Meta:
-        fields =('id','rollno','mobile','branch','email','sex','age')
+        fields =('id','rollno','mobile','branch','email','sex','dob')
 
 
 userschema = User_schema()
@@ -42,13 +44,17 @@ def reg():
         branch=request.json['branch']
         email=request.json['email']
         sex=request.json['sex']
-        age=request.json['age']
+        dob=request.json['dob']
+        name=request.json['name']
+
+        y,m,d=dob.split('-')
+        dob=datetime.datetime(int(y),int(m),int(d))
 
         if(User.query.get(rollno)):
             return make_response("You are already Registered",400)
 
-        if(rollno[-2:].isalpha() and rollno[0].isalpha() and rollno[1:7].isdigit() and mobile.isdigit() and age>=18 and age<=20 ):
-            new=User(rollno=rollno,mobile=mobile,branch=branch,email=email,sex=sex,age=age)
+        if(rollno[-2:].isalpha() and rollno[0].isalpha() and rollno[1:7].isdigit() and mobile.isdigit() ):
+            new=User(rollno=rollno,mobile=mobile,branch=branch,email=email,sex=sex,dob=dob,name=name)
             db.session.add(new)
             db.session.commit()
 
